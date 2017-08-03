3b78c77560b27d1214e68d40a615d35e628002de
AfroMonkey <moisalejandro@gmail.com>
2017-08-02 22:42:09 -0500

    [IMP] Domain

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,17 +7,19 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm(self):
+        payment_term_credits = [payment for payment in self.env['account.payment.term'].search([(1, '=', 1)]) if payment.line_ids[0] and payment.line_ids[0].days > 0]
+        payment_term_credits_ids = [p.id for p in payment_term_credits]
         for order in self:
-            payment_term_credits = [payment for payment in self.env['account.payment.term'].search([(1, '=', 1)]) if payment.line_ids[0] and payment.line_ids[0].days > 0]
             if order.payment_term_id in payment_term_credits:
+                invoices = self.env['account.invoice'].search([('partner_id', '=', order.partner_id.id), ('state', '=', 'open'), ('payment_term_id', 'in', payment_term_credits_ids)])
                 if not order.partner_id.expired_ignore:
-                    expired = self.env['account.invoice'].search([('partner_id', '=', order.partner_id.id), ('date_due', '<=', fields.Date.today()), ('state', 'not in', ['cancel', 'paid'])])
-                    if expired and len(expired) > 0:
-                        raise exceptions.Warning(_("¡POR EL MOMENTO NO SE PERMITE VENDER A CREDITO. EL CLIENTE TIENE SALDO VENCIDO EN FACTURAS ANTERIORES. PARA MAYOR INFORMACIÓN CONSULTAR EN CONTABILIDAD!"))
+                    for invoice in invoices:
+                        if invoice.date_due <= fields.Date.today():
+                            raise exceptions.Warning(_("¡POR EL MOMENTO NO SE PERMITE VENDER A CRÉDITO. EL CLIENTE TIENE SALDO VENCIDO EN FACTURAS ANTERIORES. PARA MAYOR INFORMACIÓN CONSULTAR EN CONTABILIDAD!"))
                 if not order.partner_id.credit_ignore:
                     credit = order.amount_total*order.pricelist_id.currency_id.rate
-                    for invoice in self.env['account.invoice'].search([('partner_id', '=', order.partner_id.id), ('date_due', '>', fields.Date.today()), ('state', 'not in', ['cancel', 'paid'])]):
+                    for invoice in invoices:
                         credit += invoice.amount_total*invoice.currency_id.rate
                     if credit > order.partner_id.credit_limit:
-                        raise exceptions.Warning(_("¡EL CLIENTE NO CUENTA CON CREDITO SUFICIENTE PARA ESTA VENTA, PARA MAYOR INFORMACIÓN CONSULTAR EN CONTABILIDAD!"))
+                        raise exceptions.Warning(_("¡EL CLIENTE NO CUENTA CON CRÉDITO SUFICIENTE PARA ESTA VENTA, PARA MAYOR INFORMACIÓN CONSULTAR EN CONTABILIDAD!"))
         super(SaleOrder, self).action_confirm()
